dd4f372a86a810c3f98d6351503306601816fd0c
Pablo Quijano <paquijar@gmail.com>
2023-01-20 00:08:10 -0500

    Test commit

--- app/sections/hero/Hero.tsx
+++ app/sections/hero/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
       <div className="container mx-auto py-32 text-center">
         <h1 className="text-4xl font-medium">Welcome to my portfolio</h1>
         <p className="text-xl">
-          I am Pablo Quijano Jaramillo, a software developer
+          I am Pablo Quijano Jaramillo, a software developer test
         </p>
       </div>
     </section>
